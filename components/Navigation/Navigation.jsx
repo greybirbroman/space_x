1d3ff8fad220d23c8cc691eed7dc5b1b49b5bfd5
greybirbroman <98167513+greybirbroman@users.noreply.github.com>
2023-07-25 22:49:48 +0300

    Navigation / add outsideMenuClick functionality

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -1,48 +1,70 @@
 'use client';
 import styles from './Navigation.module.css';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { navLinks } from '@/utils/constants';
 import useIsMobileResolution from '@/utils/hooks/useIsMobileResolution';
 
-const NavList = ({ className }) => {
-    return (
-        <ul className={className}>
-            {navLinks.map((item) => (
-                <li key={item.id} className={styles.navItem}>
-                    <Link href={item.link}>{item.title}</Link>
-                </li>
-            ))}
-        </ul>
-    )
-}
+const NavList = ({ className, onClick }) => {
+  return (
+    <ul className={className}>
+      {navLinks.map((item) => (
+        <li key={item.id} className={styles.navItem}>
+          <Link href={item.link} onClick={onClick}>
+            {item.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const Navigation = () => {
   const isBurgerResolution = useIsMobileResolution(1280);
-  const isMobileResolution = useIsMobileResolution(767)
+  const isMobileResolution = useIsMobileResolution(767);
   const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleBurgerMenu = () => {
-    setIsMenuVisible((prevState) => !prevState)
-  }
+    setIsMenuVisible((prevState) => !prevState);
+  };
+
+  const handleClickOutsideMenu = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsMenuVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutsideMenu);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutsideMenu);
+    };
+  }, []);
 
   return (
-    <nav>
+    <nav ref={menuRef}>
       {isBurgerResolution ? (
-          <>
+        <>
           <Image
             src='/burger-menu_white.svg'
             alt='Menu-Burger, Menu icon'
-            style={{cursor: 'pointer'}}
             width={isMobileResolution ? 20 : 40}
             height={isMobileResolution ? 20 : 40}
             onClick={toggleBurgerMenu}
+            className={styles.burgerImage}
           />
-          {isMenuVisible && <NavList className={styles.burgerMenuList}/>}
+          {isMenuVisible && (
+            <NavList
+              className={styles.burgerMenuList}
+              onClick={toggleBurgerMenu}
+            />
+          )}
         </>
       ) : (
-       <NavList className={styles.navList}/>
+        <NavList className={styles.navList} />
       )}
     </nav>
   );
